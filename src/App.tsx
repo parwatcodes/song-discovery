0b327c9efe295bb324c760798920e81fb72613b5
Parwat Kunwar <parwatkunwar08@gmail.com>
2025-01-26 17:02:09 -0400

    Implement lazy loading in Routes and Home page (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Layout from './Layout';
+import Loader from './components/shared/Loader';
 
-import Home from './pages/Home';
-import Artist from './pages/Artist';
-import Favorite from './pages/Favorite';
-import AlbumDetails from './pages/AlbumDetails';
-import ArtistAlbums from './pages/ArtistAlbums';
+const Home = lazy(() => import('./pages/Home'));
+const Artist = lazy(() => import('./pages/Artist'));
+const Favorite = lazy(() => import('./pages/Favorite'));
+const AlbumDetails = lazy(() => import('./pages/AlbumDetails'));
+const ArtistAlbums = lazy(() => import('./pages/ArtistAlbums'));
 
 import { GlobalStyles } from './styles/GlobalStyles';
 
@@ -17,6 +18,7 @@ const App: React.FC = () => {
     <Router>
       <GlobalStyles />
       <Layout />
+      <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/albums/:id" element={<AlbumDetails />} />
@@ -24,6 +26,7 @@ const App: React.FC = () => {
         <Route path="/artists/:id/albums" element={<ArtistAlbums />} />
         <Route path="/favorites" element={<Favorite />} />
       </Routes>
+      </Suspense>
     </Router>
   );
 };
