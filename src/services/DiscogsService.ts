f24e71f93a3de8997ef9aeaf5618617ec35e712c
Parwat Kunwar <parwatkunwar08@gmail.com>
2025-01-26 20:40:04 -0400

    Refactor discogs service;

--- src/services/DiscogsService.ts
+++ src/services/DiscogsService.ts
@@ -1,62 +1,45 @@
-import { getStringifiedQuery } from '../utils/helpers'
+import { getStringifiedQuery } from '../utils/helpers';
 
 const DISCOGS_API_KEY = import.meta.env.VITE_DISCOGS_API_KEY;
 const DISCOGS_API_SECRET = import.meta.env.VITE_DISCOGS_API_SECRET;
 const DISCOGS_API_BASE_URL = import.meta.env.VITE_DISCOGS_BASE_URL;
 
 class DiscogsService {
+  private static baseUrl = `${DISCOGS_API_BASE_URL}`;
 
-  private searchUrl = `${DISCOGS_API_BASE_URL}/database/search`;
+  private static appendSecrets(url: string) {
+    return `${url}&key=${DISCOGS_API_KEY}&secret=${DISCOGS_API_SECRET}`;
+  }
 
   private static async handleResponse(response: Response) {
     if (!response.ok) {
       const error = await response.json();
       throw new Error(error.message || 'Something went wrong');
     }
+
     return response.json();
   }
 
   static async search(query: Record<string, string> = {}) {
-    try {
-      const q = getStringifiedQuery(query);
+    const q = getStringifiedQuery(query);
+    const url = this.appendSecrets(`${this.baseUrl}/database/search?${q}`);
+    const response = await fetch(url);
 
-      const response = await fetch(`${DISCOGS_API_BASE_URL}/database/search?${q}&key=${DISCOGS_API_KEY}&secret=${DISCOGS_API_SECRET}`)
-
-      return await this.handleResponse(response);
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
+    return this.handleResponse(response);
   }
 
-  static getAlbumDetails = async (releaseId) => {
-    try {
-      const response = await fetch(`${DISCOGS_API_BASE_URL}/releases/${releaseId}?key=${DISCOGS_API_KEY}&secret=${DISCOGS_API_SECRET}`);
+  static getAlbumDetails = async (releaseId: string) => {
+    const response = await fetch(`${this.baseUrl}/releases/${releaseId}`);
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      return await this.handleResponse(response);
-    } catch (error) {
-      console.error('Error fetching album details:', error);
-    }
+    return this.handleResponse(response);
   };
 
-  static getArtistAlbums = async (artistId, query) => {
-    try {
-      const q = getStringifiedQuery(query);
+  static getArtistAlbums = async (artistId: string, query: Record<string, string> = {}) => {
+    const q = getStringifiedQuery(query);
+    const url = this.appendSecrets(`${this.baseUrl}/artists/${artistId}/releases?${q}`);
+    const response = await fetch(url);
 
-      const response = await fetch(`${DISCOGS_API_BASE_URL}/artists/${artistId}/releases?${q}&key=${DISCOGS_API_KEY}&secret=${DISCOGS_API_SECRET}`);
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      return await this.handleResponse(response);
-    } catch (error) {
-      console.error('Error fetching album details:', error);
-    }
+    return this.handleResponse(response);
   };
 }
 
