571114fa67d139ba5bf16c88a031bffacdd1c936
Parwat Kunwar <parwatkunwar08@gmail.com>
2025-01-27 22:19:27 -0400

    Hotfix: use hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Layout from './Layout';
 import Loader from './components/shared/Loader';
